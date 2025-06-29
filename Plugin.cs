402b22f9ca76c120215969703cc536c9eb7952de
Antoniofo <antonio57201@gmail.com>
2025-06-29 16:41:18 +0200

    Fix escaping pocket after being revived in it

--- Plugin.cs
+++ Plugin.cs
@@ -4,7 +4,10 @@ using Desfribalator;
 using Exiled.API.Enums;
 using Exiled.API.Features;
 using Exiled.CustomItems.API.Features;
+using Exiled.Events.EventArgs.Player;
+using MapGeneration;
 using MEC;
+using UnityEngine;
 
 namespace Defibrillator
 {
@@ -13,7 +16,7 @@ namespace Defibrillator
         public override string Name => "Defibrillator";
         public override string Prefix => "defibrillator";
         public override string Author => "@dzarenafixer";
-        public override Version Version { get; } = new Version(1, 4, 2);
+        public override Version Version { get; } = new Version(1, 4, 3);
         public override Version RequiredExiledVersion { get; } = new Version(9, 6, 0);
         public override PluginPriority Priority => PluginPriority.Default;
         public EventHandler EventHandlers;
@@ -28,6 +31,7 @@ namespace Defibrillator
             EventHandlers = new EventHandler();
             Exiled.Events.Handlers.Server.RoundStarted += EventHandlers.OnStart;
             Exiled.Events.Handlers.Server.RoundEnded += EventHandlers.OnRoundEnd;
+            Exiled.Events.Handlers.Player.EscapingPocketDimension += Pocket;
 
             CustomItem.RegisterItems();
         }
@@ -36,10 +40,18 @@ namespace Defibrillator
         {
             Exiled.Events.Handlers.Server.RoundStarted -= EventHandlers.OnStart;
             Exiled.Events.Handlers.Server.RoundEnded -= EventHandlers.OnRoundEnd;
+            Exiled.Events.Handlers.Player.EscapingPocketDimension -= Pocket;
             EventHandlers = null;
             Instance = null;
 
             CustomItem.UnregisterItems();
+        }
+
+        private void Pocket(EscapingPocketDimensionEventArgs ev)
+        {
+            if(ev.TeleportPosition.TryGetRoom(out RoomIdentifier room))
+                if (room.Name == RoomName.Pocket)
+                    ev.TeleportPosition = Room.Random().Position;
         }
     }
 }
